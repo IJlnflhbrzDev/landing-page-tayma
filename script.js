1d5e1f3cd394ca910ce48d084b7900c18d4802a8
IJlal Naufal Hibrizi <IJlnflhbrzDev@gmail.com>
2023-02-24 09:52:44 +0700

    test

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
 // $(document).ready(function(){
 //     $(".owl-carousel").owlCarousel();
 //   });
-  $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    responsiveClass:true,
-    lazyLoad: true , 
-    responsive:{
-        0:{
-            items:2,
-            nav:true
-        },
-        600:{
-            items:3,
-            nav:false
-        },
-        1000:{
-            items:3,
-            nav:true,
-            loop:false
-        }
-    }
-});
